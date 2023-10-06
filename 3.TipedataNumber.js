9829349c1a01ac920bf0a007f2022d8ce0214283
alriansyah <alriansyah1218@gmail.com>
2023-10-06 17:33:03 +0700

    Tipe data number

---
+++ 3.TipedataNumber.js
@@ -0,0 +1,9 @@
+// Semua angka merupakan tipe data number, baik itu integer, float, dll
+
+const bulat = 10;
+const pecahan = 10.0;
+const binary = 0b1011111;
+
+console.log("bulat : ", bulat);
+console.log("pecahan : ", pecahan);
+console.log("binary : ", binary);

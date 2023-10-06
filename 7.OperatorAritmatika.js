255334b3ebd10edc391ef7025184fa8a001c84cc
alriansyah <alriansyah1218@gmail.com>
2023-10-06 17:58:19 +0700

    Operator Aritmatika

---
+++ 7.OperatorAritmatika.js
@@ -0,0 +1,17 @@
+// + : pertambahan
+// - : pengurangan
+// * : perkalian
+// / : pembagian
+// % : modulus
+// ** : pangkat / exponent
+
+
+// Operator Augmented Assignment :
+// +=, -=, *=, /=, %=, **=
+
+
+// Operator Unary :
+// + : Menandakan nilai positif (+result)
+// - : Menandakan negatif (-result)
+// ++ : Menambah 1 angka (result++)
+// -- : Mengurangi 1 angka (result--)

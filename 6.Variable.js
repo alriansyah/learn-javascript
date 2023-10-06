a053141b97b1a3eee261b70bd82940c084af8989
alriansyah <alriansyah1218@gmail.com>
2023-10-06 17:41:48 +0700

    variable

---
+++ 6.Variable.js
@@ -0,0 +1,19 @@
+const nama = "Nama saya : Al Riansyah";
+let hobby = 'Hobby saya : Game';
+var age = 20;
+
+console.log(nama);
+console.log(hobby);
+console.log(age);
+
+hobby = "Coding";
+age = 21;
+
+console.log(hobby);
+console.log(age);
+
+// let hobby = "Coding";
+var age = 21;
+
+console.log(hobby);
+console.log(age);

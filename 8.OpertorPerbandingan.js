d9f936530e1080687b3651cb173a13bc9c5cd33c
alriansyah <alriansyah1218@gmail.com>
2023-10-07 08:46:05 +0700

    Operator perbandingan

---
+++ 8.OpertorPerbandingan.js
@@ -0,0 +1,8 @@
+// > : lebih dari
+// < : kurang dari
+// >= : lebih dari sama dengan
+// <= : kurang dari sama dengan
+// == : sama dengan  (ex: 1 == "1" : true)
+// != : tidak sama dengan
+// === : identik  (ex: 1 === "1" : false)
+// !== : tidak identik

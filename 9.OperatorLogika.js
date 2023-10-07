198d4e9460d994a55b9da233f50a72be4c512648
alriansyah <alriansyah1218@gmail.com>
2023-10-07 08:55:38 +0700

    operator logika

---
+++ 9.OperatorLogika.js
@@ -0,0 +1,18 @@
+// && : And
+console.log(true && true); //true
+console.log(true && false); //false
+console.log(false && true); //false
+console.log(false && false); //false
+console.log("-----------------------");
+
+// || : Or
+console.log(true || true); //true
+console.log(true || false); //true
+console.log(false || true); //true
+console.log(false || false); //false
+console.log("-----------------------");
+
+// ! : Not
+console.log(!true); //false
+console.log(!false); //true
+console.log("-----------------------");

07875f4dd35daf79ed6aab2a651bc181554f3c7a
alriansyah <alriansyah1218@gmail.com>
2023-10-07 09:04:59 +0700

    loging / console

---
+++ 10.Console.js
@@ -0,0 +1,10 @@
+// Javascript memiliki fitur untuk melakukan logging yaitu console
+// Logging adalah mekanisme yang biasa dilakukan oleh programmer untuk menampilkan informasi
+// dari aplikasi yang sedang berjalan, tanpa harus mengganggu kerja aplikasi dan juga interaksi user
+
+// Console method :
+console.info("Hello World"); // menampilkan informasi
+console.error("Hello World"); // menampilkan error
+console.warn("Hello World"); // menampilkan warning
+console.table({name: "Al Riansyah", hobby: "Coding"}); // menampilkan informasi dalam bentuk table
+console.log("Hello World"); // sama dengan console.info("Hello World");

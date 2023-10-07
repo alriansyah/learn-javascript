cbe1b3926860df00d63606d6bc284ca787e460e5
alriansyah <alriansyah1218@gmail.com>
2023-10-07 16:05:37 +0700

    operator in

---
+++ 21.OperatorIn.js
@@ -0,0 +1,22 @@
+// in operator adalah operator yang digunakan untuk mengecek apakah
+// sebuah property ada di dalam objek atau tidak, outputnya : boolean
+// bisa jg untuk mengecek array, namu jarang digunakan pada array.
+
+const fruits = ["apple", "banana", "cherry"];
+
+const result = "anggur" in fruits;
+console.log(result);
+
+const result2 = fruits[3] in fruits;
+console.log(result2);
+
+const user = {
+    name: "Al Riansyah",
+    age: 20,
+    isAdmin: undefined
+}
+
+const check = "isAdmin" in user;
+console.log(check);
+
+// jika valuenya bernilai null atau undefined maka akan tetap di anggap ada / true

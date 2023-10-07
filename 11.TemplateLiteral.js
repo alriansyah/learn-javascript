f6593a127d9d2039274e5dbbfb0ca70febf9ad9b
alriansyah <alriansyah1218@gmail.com>
2023-10-07 09:12:05 +0700

    Template literal

---
+++ 11.TemplateLiteral.js
@@ -0,0 +1,12 @@
+// Template Literal :
+const nama = "Al Riansyah";
+console.log(`Perkenalkan nama saya : ${nama}`);
+
+const nilai = 100;
+console.log(`Anda lulus dari mata kuliah: ${nilai > 90 ? "Yes" : "No"}`);
+
+
+// String concat :
+const perkenalan = "Perkenalkan nama saya : ";
+console.log(perkenalan + nama);
+console.log("Perkenalkan nama saya : " + nama);

d86d56d242408ded6b57489fa5b651670d214d7d
alriansyah <alriansyah1218@gmail.com>
2023-10-07 15:47:57 +0700

    Switch case

---
+++ 19.SwitchCase.js
@@ -0,0 +1,16 @@
+// Switch case hanya untuk perbandingan ==
+const nilai = "C";
+
+switch (nilai) {
+    case "A":
+        console.log("Nilai sangat baik");
+        break;
+    case "B":
+        console.log("Nilai baik");
+        break;
+    case "C":
+        console.log("Nilai cukup");
+        break;
+    default:
+        console.log("Maaf anda tidak lulus");
+}

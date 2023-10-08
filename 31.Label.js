efce3a15f0ab0dc6ea2023f2a0468b0e9ec802d3
alriansyah <alriansyah1218@gmail.com>
2023-10-08 10:56:07 +0700

    Label

---
+++ 31.Label.js
@@ -0,0 +1,22 @@
+// Label :
+// - merupakan penanda yang bisa digunakan dengan kata kunci break dan continue
+
+// loopi:
+// for (let i = 0; i <= 100; i++) {
+//     loopj:
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`${i} - ${j}`);
+//     }
+// }
+
+
+loopi:
+for (let i = 0; i <= 10; i++) {
+    loopj:
+    for (let j = 0; j <= 100; j++) {
+        if (j > 10) {
+            continue loopi;
+        }
+        console.log(`${i} - ${j}`);
+    }
+}

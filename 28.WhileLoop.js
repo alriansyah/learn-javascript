e946feef7dbc5670080f4f63834a14db8a44339b
alriansyah <alriansyah1218@gmail.com>
2023-10-08 08:20:43 +0700

    Do while loop

--- 28.WhileLoop.js
+++ 28.WhileLoop.js
@@ -1,4 +1,5 @@
 // while loop lebih sederhana dari for loop
+// Pengecekan kondisi di while loop dilakukan di awal sebelum perulangan dilakukan
 let count = 1;
 while (count <= 10) {
     console.log(count);

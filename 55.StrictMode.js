da599588f5571ee019d33955ed2d921852359d73
alriansyah <alriansyah1218@gmail.com>
2023-10-12 09:10:50 +0700

    Strict Mode

---
+++ 55.StrictMode.js
@@ -0,0 +1,22 @@
+// Strict Mode :
+// - Saat kita menjalankan kode program javascript, secara default kode program kita
+// berjalan dalam mode tidak strict, atau istilahnya sloppy mode.
+// Pada ES5, diperkenan mode strict, dimana ketika mode strict dijalankan, maka akan merubah
+// beberapa cara kerja Javascript, seperti :
+// ~ Merubah beberapa javascript error dari yang tadinya silent error, menjadi throw error (terlihat).
+// ~ Memperbaiki beberapa kesalahan engine Javascript untuk optimisasi.
+// ~ Menolak beberapa kode perintah yang kedepannya akan digunakan di ECMAScript.
+
+
+// use strict pada function :
+function useStrictMode() {
+    "use strict";
+    const person = {
+        firstName: "Eko"
+    }
+    with (person) {
+        console.log(firstName);
+    }
+}
+
+useStrictMode();

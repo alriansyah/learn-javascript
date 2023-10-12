5ece9840ad55014caccd3610015baec565b36c85
alriansyah <alriansyah1218@gmail.com>
2023-10-12 09:13:59 +0700

    use strict

--- 55.StrictMode.js
+++ 55.StrictMode.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // Strict Mode :
 // - Saat kita menjalankan kode program javascript, secara default kode program kita
 // berjalan dalam mode tidak strict, atau istilahnya sloppy mode.
@@ -6,6 +8,9 @@
 // ~ Merubah beberapa javascript error dari yang tadinya silent error, menjadi throw error (terlihat).
 // ~ Memperbaiki beberapa kesalahan engine Javascript untuk optimisasi.
 // ~ Menolak beberapa kode perintah yang kedepannya akan digunakan di ECMAScript.
+
+// Meletakan Strict Mode :
+// - di awal file atau dalam function
 
 
 // use strict pada function :
